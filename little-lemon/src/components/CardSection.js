e06c7073b3a1c31a5ff5a58cc5e312f0fbe1c908
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-09-08 14:08:34 +0200

    fixed card

--- little-lemon/src/components/CardSection.js
+++ little-lemon/src/components/CardSection.js
@@ -6,12 +6,16 @@ function CardSection() {
     return (
         <>
             <div className='container max-w-full mt-12 '>
-                <div className='flex flex-row space-x-4 justify-center bg-transparent'>
+                <div className='flex flex-row space-x-4 justify-center bg-transparent pt-4'>
                     {/* Card 1 */}
-                    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg pt-4 bg-gray-200">
-                        <img className="w-full h-256.22 bg-transparent" src={greek_salad} alt="Greek Salad Image" />
+                    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-gray-200">
+                        <img className="w-full h-64 bg-transparent" src={greek_salad} alt="Greek Salad Image" />
                         <div className="px-6 pt-4">
-                            <div className="font-bold text-xl mb-2">Greek Salads</div>
+                            <div className="font-bold text-xl mb-2">Greek Salads
+                                <span className="inline-block ml-16 text-orange-400">
+                                    $12.99
+                                </span>
+                            </div>
                             <p className="text-gray-700 text-base">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                             </p>
@@ -23,10 +27,14 @@ function CardSection() {
                         </div>
                     </div>
                     {/* Card 2 */}
-                    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg pt-4  bg-gray-200">
-                        <img className="w-full bg-transparent" src={bruschetta} alt="Bruschetta Image" />
-                        <div className="px-6 py-4 ">
-                            <div className="font-bold text-xl mb-2">Bruschetta</div>
+                    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-gray-200">
+                        <img className="w-full h-64 bg-transparent" src={bruschetta} alt="Bruschetta Image" />
+                        <div className="px-6 pt-4 ">
+                            <div className="font-bold text-xl mb-2">Bruschetta
+                            <span className="inline-block ml-16 text-orange-400">
+                                    $10.99
+                                </span>
+                            </div>
                             <p className="text-gray-700 text-base">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                             </p>
@@ -38,10 +46,14 @@ function CardSection() {
                         </div>
                     </div>
                     {/* Card 3 */}
-                    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg pt-4 bg-gray-200">
-                        <img className="w-full bg-transparent" src={lemon_dessert} alt="Lemon dessert image" />
-                        <div className="px-6 py-4 ">
-                            <div className="font-bold text-xl mb-2">Lemon Dessert</div>
+                    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-gray-200">
+                        <img className="w-full h-64 bg-transparent" src={lemon_dessert} alt="Lemon dessert image" />
+                        <div className="px-6 pt-4 ">
+                            <div className="font-bold text-xl mb-2">Lemon Dessert
+                            <span className="inline-block ml-16 text-orange-400">
+                                    $14.99
+                                </span>
+                            </div>
                             <p className="text-gray-700 text-base">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                             </p>
