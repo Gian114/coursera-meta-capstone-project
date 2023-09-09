e7dd6de7251986c1194cf79cca8756322b1f9cab
Gian114 <100781136+Gian114@users.noreply.github.com>
2023-09-09 15:35:59 +0200

    updateTime

--- little-lemon/src/components/BookingForm.js
+++ little-lemon/src/components/BookingForm.js
@@ -15,7 +15,7 @@ function BookingForm() {
             <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required placeholder='Date'
                 className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline' />
             <label className='block text-gray-700 text-sm font-bold mb-2'>Choose Time:</label>
-            <select value={availableTime} onChange={(event) => setTime(event.target.value)}
+            <select value={availableTime[0]} onChange={(event) => setTime(event.target.value)}
                 className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'>
                 <option value="17">17:00</option>
                 <option value="18">18:00</option>
